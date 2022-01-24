15098c3d7878410f873c83042b6c04648d2f473e
Felix <felix@almer.dev>
2022-01-24 10:36:58 +0100

    an order shall be identifiable

--- Domain/Order.cs
+++ Domain/Order.cs
@@ -1,4 +1,5 @@
 public class Order {
+    public string OrderId { get; set; }
     public Ticket Ticket { get; set; }
     public OrderState OrderState { get; set; }
     public User Seller { get; set; }
